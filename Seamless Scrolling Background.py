321db4aa29b45d0ba5ef5d4c67eb02cb3bc1b3c5
Humanitasium <87041850+Humanitasium@users.noreply.github.com>
2021-12-31 17:44:09 +0100

    Update Seamless Scrolling Background.py

--- Seamless Scrolling Background.py
+++ Seamless Scrolling Background.py
@@ -1,22 +1,31 @@
-import pygame
-pygame.init()
+import pygame #imports the pygame module
+pygame.init() #initalizes the pygame module
 width = 800
 height= 600
-win = pygame.display.set_mode((width, height))
-bg_img = pygame.image.load("Assets/Background.png")
-bg= pygame.transform.scale(bg_img, (width,height))
+win = pygame.display.set_mode((width, height)) #sets the display size
+bg_img = pygame.image.load("Background.png") #loads the image
+bg= pygame.transform.scale(bg_img, (width,height)) #scales the image to fit the display size
 i=0
 run=True
-while run:
-    for event in pygame.event.get():
+while run: #while loop
+    for event in pygame.event.get(): #quits the program
         if event.type == pygame.QUIT:
-            run=False
-    win.blit(bg, (i,0))
-    win.blit(bg, (width+i, 0))
-    if i == -width:
+            run=False #exits the while loop
+    win.blit(bg, (i,0)) #displays the starting image
+    win.blit(bg, (width+i, 0)) 
+    '''
+    displays the image again
+    but further along to give the impression 
+    of a seamless never ending background
+    '''
+    if i == -width: 
+    '''
+    once a full loop of the image has been completed
+    the position of the image is reset
+    '''
         win.blit(bg, (i+width,0))
         i=0
-    i -= 5
-    pygame.display.flip()
-pygame.quit()
+    i -= 5 # sets the speed of the loop
+    pygame.display.flip() #updates display
+pygame.quit() 
 
